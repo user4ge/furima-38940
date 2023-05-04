db8ec36c212b5aeeb584f4d904c06c5a5fe6f822
user4ge <masumi.kurashige@gmail.com>
2023-05-04 17:26:33 +0900

    コメントアウトを削除

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user do
     nickname              { Gimei.first.hiragana }
     email                 { Faker::Internet.free_email }
-    # password: Default configuration is mix_case: true && special_characters: false
     password              { Faker::Lorem.characters(number: 8, min_alpha: 1, min_numeric: 1) }
     password_confirmation { password }
     last_name             { Gimei.last.kanji }
