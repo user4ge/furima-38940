bad4f58af5566d43252e9cf9620fa682d9547f1c
user4ge <masumi.kurashige@gmail.com>
2023-05-06 16:58:23 +0900

    アソシエーションとバリデーションを記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :category
+    belongs_to :condition
+    belongs_to :shipping_cost
+    belongs_to :prefecture
+    belongs_to :shipping_day
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :price, presence: true, format: { with: /\A[3-9]\d{2}|\d{4,7}\z/ }
+  validates :user, presence: true, foreign_key: true
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+
   validates :nickname, presence: true
   validates :password, format: { with: /\A(?=.*?[A-Za-z])(?=.*?\d)[A-Za-z\d]+\z/ }
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, allow_blank: true } do
