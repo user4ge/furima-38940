cdf3fb64ec8dd091a022cd451e83bd220d294fd6
user4ge <masumi.kurashige@gmail.com>
2023-05-06 19:08:19 +0900

    バリデーションの追記

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,14 +10,14 @@ class Item < ApplicationRecord
     belongs_to :shipping_day
 
   validates :image, presence: true
-  validates :name, presence: true
-  validates :description, presence: true
+  validates :name, presence: true, length: { maximum: 40 }
+  validates :description, presence: true, length: { maximum: 1000 }
   validates :category_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_cost_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :shipping_day_id, numericality: { other_than: 1 , message: "can't be blank"}
   validates :price, presence: true, format: { with: /\A[3-9]\d{2}|\d{4,7}\z/ }
-  validates :user, presence: true, foreign_key: true
+  validates :user, presence: true
 
 end
