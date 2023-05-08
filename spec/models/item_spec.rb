40599726a91b4d1ea7096ab899422e3ddf2603d2
user4ge <masumi.kurashige@gmail.com>
2023-05-08 14:47:19 +0900

    テストコードの追加と修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -70,22 +70,27 @@ RSpec.describe Item, type: :model do
       it 'priceが300未満では出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is invalid')
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
       end
       it 'priceが10_000_000以上では出品できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is invalid')
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
       it 'priceが英字を含むと出品できない' do
         @item.price = 'abcdef'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is invalid')
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'priceが全角を含むと出品できない' do
         @item.price = '１２３４５６'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price is invalid')
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it 'userが紐づいていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
       end
     end
   end
