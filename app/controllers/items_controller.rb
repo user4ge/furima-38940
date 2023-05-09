0e5acb20a55bc5b36f01d290bfcf30100d515b9c
user4ge <masumi.kurashige@gmail.com>
2023-05-09 18:20:29 +0900

    edit, updateアクションと遷移の制限を設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :set_item, only: [:edit, :show]
+  before_action :correct_item, only: [:edit]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -19,12 +21,33 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    item = Item.find(params[:id])
+    if item.update(item_params)
+      redirect_to item_path(item)
+    else
+      render :edit
+    end
   end
 
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def correct_item
+    unless @item.user.id == current_user.id
+      redirect_to root_path
+    end
   end
 end
