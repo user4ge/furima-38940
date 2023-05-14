c3eee3247ecb02ee959ecd76eb0d76b1c5244478
user4ge <masumi.kurashige@gmail.com>
2023-05-14 17:38:21 +0900

    createアクションを記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,27 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index, :create]
+
   def index
-    @item = Item.find(params[:item_id])
     @order_shipping = OrderShipping.new
   end
 
   def create
+    @order_shipping = OrderShipping.new(order_params)
+    if @order_shipping.valid?
+      @order_shipping.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def order_params
+    params.require(:order_shipping).permit(:postcode, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: @item.id)
   end
 end
