6712cc7a32bdcf3e92b77f0272c9c25021ccc5a6
user4ge <masumi.kurashige@gmail.com>
2023-05-20 18:19:38 +0900

    /ordersの直接入力対策

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!, only: [:index]
   before_action :set_item, only: [:index, :create]
+  before_action :move_to_index, only: [:index]
 
   def index
     @order_shipping = OrderShipping.new
@@ -20,6 +22,12 @@ class OrdersController < ApplicationController
   
   def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def move_to_index
+    if @item.order != nil || @item.user_id == current_user.id
+      redirect_to root_path
+    end
   end
 
   def order_params
