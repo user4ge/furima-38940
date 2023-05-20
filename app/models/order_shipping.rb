033ffbdaf2b72ebc3e6aa716491710b3601c60d3
user4ge <masumi.kurashige@gmail.com>
2023-05-20 16:19:27 +0900

    tokenの値をorder_shippingに追加

--- app/models/order_shipping.rb
+++ app/models/order_shipping.rb
@@ -1,6 +1,6 @@
 class OrderShipping
   include ActiveModel::Model
-  attr_accessor :postcode, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id
+  attr_accessor :postcode, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postcode, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
